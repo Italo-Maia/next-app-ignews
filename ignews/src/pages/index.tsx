2ee2b0a2791d3c8c5b73da314d300f6419225859
Italo-Maia <italomaiatech@gmail.com>
2022-03-27 17:22:24 -0300

    Adicionando SASS e Typescript

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+import styles from '../../styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <div>
+     <h1 className={styles.title}>
+        Italo <span> Maia </span>
+       </h1>
+    </div>
+  )
+}
